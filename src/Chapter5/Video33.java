f824fd607ca7df1190b5a1aa968f7b1b38153bea
Liam <phuc01112002@gmail.com>
2024-06-24 22:04:50 +0700

    áºarrayList

---
+++ src/Chapter5/Video33.java
@@ -0,0 +1,17 @@
+package Chapter5;
+
+import java.util.ArrayList;
+
+public class Video33 {
+	public static void main(String[] args) {
+		System.out.println("Run video 33");
+		int a = 10;
+		ArrayList<String> a1 = new ArrayList<String>();
+//		a1.add(2);
+		a1.add("Phúc liam");
+		a1.add("Phúc đẹp trai liam");
+
+		System.out.println(a1.toString());
+		System.out.println("Size is: " + a1.size());
+	}
+}
